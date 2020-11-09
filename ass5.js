8852c9434b0c3a2c714fcb7b98f5934996841735
dubs97 <62757503+dubs97@users.noreply.github.com>
2020-11-08 21:35:12 -0500

    assignement 6  new files

--- ass5.js
+++ ass5.js
@@ -1,46 +1,18 @@
 var productArr = []
+var productArr2 = []
+
 
 class Product {
     
-    constructor(quantity,flavor) {
+    constructor(type,quantity,flavor,total) {
+        this.type = type
         this.quantity = quantity
         this.flavor = flavor 
+        this.total = total
 
     }
 } 
 
-
-function addToCart() {
-    alert("Added to Cart")
-
-    var quantity = document.getElementById('quantity').value 
-
-    var flavors = document.getElementsByName('flavor') ;
-
-    var selectedflavor = "blank";
-
-    for (var i=0; i < flavors.length; i++) {
-         if(flavors[i].checked) {
-                selectedflavor = flavors[i].value;
-         }
-
-    }
-
-    var bun = new Product(quantity,selectedflavor)
-
-    productArr.push(bun)
-
-    updateCartNumber(productArr.length)
-
-}
-
-function updateCartNumber(num) {
-    var cartCount = document.getElementById('cartCount')
-    cartCount.innerHTML = num
-
-}
-
-let productPageSubtotal = document.getElementById("Subtotal");
 
 function updateProductSubtotal() {
     let productSubtotal = 0;
@@ -60,6 +32,112 @@ function updateProductSubtotal() {
    
     productPageSubtotal.innerHTML = productSubtotal;
 
-    console.log(productSubtotal);
+}
+
+function cartLoaded() {
+
+    var loadedProductArr = localStorage.getItem('order')
+    var productArr2 = JSON.parse(loadedProductArr)
+
+    var listOfProducts = document.getElementById('listOfProducts')
+	
+	for(var i = 0; i < productArr2.length; i++) {
+       var bun = productArr2[i]
+       var bunType = bun.type
+	   var bunQuantity = bun.quantity
+       var bunFlavor = bun.flavor
+       var bunTotal = bun.total
+		listOfProducts.innerHTML += '<div <ul class="ps">' + '<li id="a1"</li>'  + bun.type + '<li id="a1"</li> Quantity: ' + bun.quantity+ '<li id="a2"</li> Flavor: ' + bun.flavor + '<li id="a3"</li> Total: '+ bun.total + '<span class="vp" id="a8" onclick="deleteProduct(' + i + ')">Delete item</span>'+ '</li></div>' 
+		listOfProducts.innerHTML += '<br /><br /><br />'
+	   }
+	   
+	   
+	}
+
+function saveEdits() {
+
+    localStorage.setItem('order', JSON.stringify(productArr2))
+    }
+    
+function deleteProduct(i) {
+    var loadedProductArr = localStorage.getItem('order')
+	productArr2 = JSON.parse(loadedProductArr)
+    alert('Deleting item')
+    console.log('before we delete')
+    console.log(productArr2)
+        
+    productArr2.splice(i,1)
+        
+    console.log('after we delete')
+    console.log(productArr2)
+        
+    listOfProducts.innerHTML = ''
+        
+    for(var i = 0; i < productArr2.length; i++) {
+        var bun = productArr2[i]
+        var bunType = bun.type
+        var bunQuantity = bun.quantity
+        var bunFlavor = bun.flavor
+        var bunTotal = bun.total
+         listOfProducts.innerHTML += '<div <ul class="ps">' + '<li id="a1"</li>'  + bun.type + '<li id="a1"</li> Quantity: ' + bun.quantity+ '<li id="a2"</li> Flavor: ' + bun.flavor + '<li id="a3"</li> Total: '+ bun.total + '<span class="vp" id="a8" onclick="deleteProduct(' + i + ')">Delete item</span>' + '</li></div>' 
+         listOfProducts.innerHTML += '<br /><br /><br />'
+        }
+    
+
+ 
+    }
+    
+
+function addToCart() {
+
+    alert("Added to Cart")
+
+    var producttype = document.getElementById("Type").innerHTML ; 
+
+    var flavors = document.getElementsByName('flavor') ;
+
+    var selectedflavor = "blank";
+
+    for (var i=0; i < flavors.length; i++) {
+         if(flavors[i].checked) {
+                selectedflavor = flavors[i].value;
+         }
+
+    }
+
+
+    let amountValue = document.querySelector('input[name="quant"]:checked').value;
+
+    var totalAmt = document.getElementById("Subtotal").innerHTML ;
+
+    console.log(productArr)
+    var bun = new Product(producttype,amountValue,selectedflavor,totalAmt)
+
+    productArr.push(bun)
+
+    updateCartNumber(productArr.length)
+
+}
+
+function updateCartNumber(num) {
+    var cartCount = document.getElementById('cartCount')
+    cartCount.innerHTML = num
+
+}
+
+let productPageSubtotal = document.getElementById("Subtotal");
+
+function saveEdits2() {
+
+    localStorage.setItem('order', JSON.stringify(productArr))
+    }
+
+function goToCartPage() {
+    
+    localStorage.setItem('order', JSON.stringify(productArr))
+
+    window.location.replace("cart.html")
+
+
 }
 
